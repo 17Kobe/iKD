7d09ba37216c735ec76496148715ed5b09fe7f85
kobe <kobe@cht.com.tw>
2022-08-18 09:04:49 +0800

    成本線改實線

--- src/components/ChartWeekK.vue
+++ src/components/ChartWeekK.vue
@@ -348,7 +348,7 @@ export default {
                         name: '成本線',
                         color: '#4286f5',
                         data: this.cost,
-                        dashStyle: 'ShortDash',
+                        // dashStyle: 'ShortDash',
                         step: 'left',
                         dataGrouping: {
                             // anchor: 'end',
