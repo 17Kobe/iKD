06c488725557b8c39c11a1a872d2431302df7953
kobe <kobe@cht.com.tw>
2023-04-24 12:44:12 +0800

    不要用 prebuild

--- scheduled_deploy.bat
+++ scheduled_deploy.bat
@@ -8,7 +8,12 @@ git pull https://github.com/17kobe/iKD.git gh-pages --force
 cd ..
 git pull origin master --force
 node updateJsonFile.js
+:: 同 mkdir dist-temp && cp -r dist/assets/images dist/assets/data dist-temp/
+New-Item -ItemType Directory -Force -Path ".\dist-temp" ; Copy-Item -Path ".\dist\assets\images" ," .\dist\assets\data" -Destination ".\dist-temp\" -Recurse
 call npm run build
+:: 同 mv dist-temp/images dist-temp/data dist/assets/ && rm -rf dist-temp
+Move-Item -Path dist-temp/images, dist-temp/data -Destination dist/assets/
+Remove-Item -Path dist-temp -Recurse
 :: curl --proxy http://10.160.3.88:8080 -o ./dist/assets/data/my_localstorage.json https://17kobe.github.io/iKD/assets/data/my_localstorage.json
 cd /d "D:\Code\iKD\dist\"
 git add -A
