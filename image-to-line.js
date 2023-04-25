29990ba338a8199771d6dfffbd66c86aa5ba0c24
kobe <kobe@cht.com.tw>
2023-04-25 08:46:23 +0800

    送給 line 改成有 axios proxy，不然會 connection timeout

--- image-to-line.js
+++ image-to-line.js
@@ -9,6 +9,10 @@ const config = {
         'Content-Type': 'application/json',
         Authorization: `Bearer GW2r4UtoAtIsU0XVCBUp33fmbVCy+F6l7oDRkKE+/kAYoU2OFt/EGLviboXIwnx4N8/7OA1KyN6X9GVUNB6zMzBoR6gjGPNUIAZkyZYTjIbs/qP9qprMw6iL+O9/+6ZAupUYB/5jkPwQbxGn9rKLUQdB04t89/1O/w1cDnyilFU=`,
     },
+    proxy: {
+        host: '10.160.3.88',
+        port: '8080'
+    }
 };
 
 const data = {
