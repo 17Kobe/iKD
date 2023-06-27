99de9a754b81fec6df5c5a54f267aff14a74b9ce
kobe <kobe701028@gmail.com>
2023-06-27 20:13:15 +0800

    today

--- src/views/AssetAllocation.vue
+++ src/views/AssetAllocation.vue
@@ -9,7 +9,7 @@
             <el-col :xs="12" :sm="10" :md="7" :lg="4" :xl="3" style="display: flex; padding: 4px 4px 0 2px">
                 <el-card shadow="hover" style="flex: 1" id="line-chart-card">
                     <div style="font-size: 12px; text-align: center; font-weight: bold; margin-top: 2px; color: #6c6c6c">
-                        資產走勢 (今日增減: $ {{ todayAsset.toLocaleString('en-US') }})
+                        資產走勢 (今日: $ {{ todayAsset.toLocaleString('en-US') }})
                     </div>
                     <el-tooltip
                         class="box-item"
