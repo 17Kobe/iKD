a743c158b8c410c9d580aa865776aa9f649d2791
kobe <kobe@cht.com.tw>
2023-09-01 09:00:47 +0800

    買取消

--- src/views/ListStock.vue
+++ src/views/ListStock.vue
@@ -28,7 +28,7 @@
                             :value="scope.row.badge"
                             class="item"
                             :class="[['買', '買x2', '賣', '賣½'].includes(scope.row.badge) ? 'shake-base' : '', 'item']"
-                            :type="['買', '買x2', '準買', '準買x2'].includes(scope.row.badge) ? 'danger' : 'success'"
+                            :type="['買', '買x2', '準買', '準買x2'].includes(scope.row.badge) ? 'danger' : ['買取消', '買x2取消', '賣取消', '賣½取消'].includes(scope.row.badge) ? 'info' : 'success'"
                         >
                             <span
                                 style="font-size: 16px; font-weight: bold; cursor: pointer"
